43f711b27fc87056a0af3c1ade5d4bb78092b517
Afonso Carvalho <afonso.martingo.c@gmail.com>
2025-03-09 20:23:41 +0000

    Add decision tree implementations and exam

--- utils.py
+++ utils.py
@@ -1,44 +1,118 @@
-# C2_W1 Utilities
-import numpy as np
+from PIL import Image
+import networkx as nx
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+from networkx.drawing.nx_pydot import graphviz_layout
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def generate_node_image(node_indices):
+    image_paths = ["images/%d.png" % idx for idx in node_indices]
+    images = [Image.open(x) for x in image_paths]
+    widths, heights = zip(*(i.size for i in images))
 
-# Plot  multi-class training points
-def plot_mc_data(X, y, class_labels=None, legend=False,size=40):
-    classes = np.unique(y)
-    for i in classes:
-        label = class_labels[i] if class_labels else "class {}".format(i)
-        idx = np.where(y == i)
-        plt.scatter(X[idx, 0], X[idx, 1],  cmap=plt.cm.Paired,
-                    edgecolor='black', s=size, label=label)
-    if legend: plt.legend()
-        
+    total_width = sum(widths)
+    max_height = max(heights)
 
-#Plot a multi-class categorical decision boundary
-# This version handles a non-vector prediction (adds a for-loop over points)
-def plot_cat_decision_boundary(X,predict , class_labels=None, legend=False, vector=True):
+    new_im = Image.new('RGB', (total_width, max_height))
 
-    # create a mesh to points to plot
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    h = max(x_max-x_min, y_max-y_min)/200
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    points = np.c_[xx.ravel(), yy.ravel()]
+    x_offset = 0
+    for im in images:
+        new_im.paste(im, (x_offset,0))
+        x_offset += im.size[0]
+    
+    new_im = new_im.resize((int(total_width*len(node_indices)/10), int(max_height*len(node_indices)/10)))
+    
+    return new_im
 
-    #make predictions for each point in mesh
-    if vector:
-        Z = predict(points)
-    else:
-        Z = np.zeros((len(points),))
-        for i in range(len(points)):
-            Z[i] = predict(points[i].reshape(1,2))
-    Z = Z.reshape(xx.shape)
 
-    #contour plot highlights boundaries between values - classes in this case
-    plt.figure()
-    plt.contour(xx, yy, Z, colors='g') 
-    plt.axis('tight')
+def generate_split_viz(node_indices, left_indices, right_indices, feature):
+    
+    G=nx.DiGraph()
+    
+    indices_list = [node_indices, left_indices, right_indices]
+    for idx, indices in enumerate(indices_list):
+        G.add_node(idx,image= generate_node_image(indices))
+
+    G.add_edge(0,1)
+    G.add_edge(0,2)
+
+    pos = graphviz_layout(G, prog="dot")
+
+    fig=plt.figure()
+    ax=plt.subplot(111)
+    ax.set_aspect('equal')
+    nx.draw_networkx_edges(G,pos,ax=ax, arrows=True, arrowsize=40)
+    
+    trans=ax.transData.transform
+    trans2=fig.transFigure.inverted().transform
+
+    feature_name = ["Brown Cap", "Tapering Stalk Shape", "Solitary"][feature]
+    ax_name = ["Splitting on %s" % feature_name , "Left: %s = 1" % feature_name, "Right: %s = 0" % feature_name]
+    for idx, n in enumerate(G):
+        xx,yy=trans(pos[n]) # figure coordinates
+        xa,ya=trans2((xx,yy)) # axes coordinates
+        piesize = len(indices_list[idx])/9
+        p2=piesize/2.0
+        a = plt.axes([xa-p2,ya-p2, piesize, piesize])
+        a.set_aspect('equal')
+        a.imshow(G.nodes[n]['image'])
+        a.axis('off')
+        a.set_title(ax_name[idx])
+    ax.axis('off')
+    plt.show()
+    
+    
+def generate_tree_viz(root_indices, y, tree):
+    
+    G=nx.DiGraph()
+    
+    
+    G.add_node(0,image= generate_node_image(root_indices))
+    idx = 1
+    root = 0
+    
+    num_images = [len(root_indices)]
+    
+    feature_name = ["Brown Cap", "Tapering Stalk Shape", "Solitary"]
+    y_name = ["Poisonous","Edible"]
+    
+    decision_names = []
+    leaf_names = []
+    
+    for i, level in enumerate(tree):
+        indices_list = level[:2]
+        for indices in indices_list:
+            G.add_node(idx,image= generate_node_image(indices))
+            G.add_edge(root, idx)
+            
+            # For visualization
+            num_images.append(len(indices))
+            idx += 1
+            if i > 0:
+                leaf_names.append("Leaf node: %s" % y_name[max(y[indices])])
+            
+        decision_names.append("Split on: %s" % feature_name[level[2]])
+        root += 1
+    
+    
+    node_names = decision_names + leaf_names
+    pos = graphviz_layout(G, prog="dot")
+
+    fig=plt.figure(figsize=(14, 10))
+    ax=plt.subplot(111)
+    ax.set_aspect('equal')
+    nx.draw_networkx_edges(G,pos,ax=ax, arrows=True, arrowsize=40)
+    
+    trans=ax.transData.transform
+    trans2=fig.transFigure.inverted().transform
+
+    for idx, n in enumerate(G):
+        xx,yy=trans(pos[n]) # figure coordinates
+        xa,ya=trans2((xx,yy)) # axes coordinates
+        piesize = num_images[idx]/25
+        p2=piesize/2.0
+        a = plt.axes([xa-p2,ya-p2, piesize, piesize])
+        a.set_aspect('equal')
+        a.imshow(G.nodes[n]['image'])
+        a.axis('off')
+        a.set_title(node_names[idx], y=-0.8, fontsize=13, loc="left")
+    ax.axis('off')
+    plt.show()
